dd85d4970175298a7d097637f2209dd4a454c356
Juan Carlos Concha <jnkarlos.c@gmail.com>
2020-09-21 00:46:09 -0300

    added views to posts

--- app/adapters/jsonplaceholder/post.rb
+++ app/adapters/jsonplaceholder/post.rb
@@ -7,6 +7,8 @@ module Jsonplaceholder
       response = self.class.get("/posts?_star#{start}t=&_limit=#{limit}")
       posts = JSON.parse(response.body)
       add_hash_count_comments(posts)
+      add_hash_name(posts)
+      add_hash_comments(posts)
     end
 
 
@@ -14,7 +16,9 @@ module Jsonplaceholder
       response = self.class.get("/posts/#{post_id}")
       post = JSON.parse(response.body)
       post['count_comments'] = add_hash_count_comments(post)
-
+      post['comments'] = add_hash_comments(post)
+      post['user_name'] = add_hash_name(post)
+      
       post
     end
 
@@ -37,14 +41,42 @@ module Jsonplaceholder
           post['count_comments'] = count_comments(post['id'])
         end
       else
-        object['count_comments'] = count_comments(object['id'])
+        count_comments(object['id'])
       end
+    end
+
+    def add_hash_name(object)
+      jsonplaceholder = Jsonplaceholder::User.new
+
+      if object.is_a?(Array)
+        object.each do |post|
+          user = jsonplaceholder.find(post['userId'])
+          post['user_name'] = user['name']
+        end
+      else
+        user = jsonplaceholder.find(object['userId'])
+        user['name']
+      end
+      
+    end
+
+    def add_hash_comments(object)
+      if object.is_a?(Array)
+        object.each do |post|
+          post['comments'] = comments(post['id'], 1, 3)
+        end
+      else
+        comments(object['id'])
+      end
+    end
+
+    def comments(post_id, start = nil, limit = nil)
+      response = self.class.get("/posts/#{post_id}/comments?_star#{start}t=&_limit=#{limit}")
+      comments = JSON.parse(response.body)
     end
     
     def count_comments(post_id)
-      response = self.class.get("/posts/#{post_id}/comments")
-      count = JSON.parse(response.body).count
-      count
+      comments(post_id).count
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :posts, only: [:index, :show]
+  resources :users, only: [:show]
+  
+  root to: 'posts#index'
+  
   namespace :api, defaults: { format: :json } do
     get '/users', to: 'users#index'
     get '/users/influencers', to: 'users#influencers'
